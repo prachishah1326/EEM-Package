a33b7bca1ff4a916bfebaac2a6fdd6f94a36e5f5
Prachi Shah - Network <shah_prachi@network.elanco.com>
2019-11-01 19:37:16 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+const moment =  require('moment');
+const todaydate = moment().subtract(12, 'M').format('YYYY-MM-DD').toString();
+
+const dateparams = `javascript:gs.dateGenerate('${todaydate}','23:59:59')`;
+
+const optionsAuth = { user: `${process.env.SNOW_USERNAME}`, password: `${process.env.SNOW_PASSWORD}` };
+
+const Client = require('node-rest-client').Client;
+
+const client = new Client(optionsAuth);
+
+function dataList( management) {
+  const group = 'ELANCOMQ-TIER2-TCS';
+  const url = `https://lilly.service-now.com/api/now/table/${management}?sysparm_display_value=true&sysparm_query=sys_created_on>${dateparams}^assignment_group.name=${group}&sysparm_fields=number,state,assigned_to,impact,priority,severity,short_description,opened_at,closed_at,calendar_duration,time_worked,assignment_group,u_ci_class_filter,cmdb_ci`;
+
+  try{
+
+    client.get(url, (data1) => {
+      const items = data1.result;
+      console.log(url);
+      console.log("data "+items);
+      console.log("item: "+JSON.parse(JSON.stringify(data1)));
+      console.log("json"+JSON.stringify(data1));
+      const reqItems = [];
+      if(items !==undefined){
+      for (let i = 0; i < items.length; i++) {
+        const row = {};
+
+        row.ticket_number = items[i].number.toString();
+
+        if (items[i].short_description) {
+          row.short_description = items[i].short_description;
+        } else {
+          row.short_description = null;
+        }
+
+        if (items[i].opened_at) {
+          row.opend_at = moment(items[i].opened_at).format('DD/MM/YYYY');
+        } else {
+          row.opend_at = null;
+        }
+        if (items[i].opened_at === items[i].closed_at) {
+          row.isOpenCloseSame = true;
+        }
+        if (items[i].state) {
+          row.state = items[i].state;
+        } else {
+          row.state = null;
+        }
+
+        if (items[i].time_worked) {
+          row.time_worked = items[i].time_worked;
+        } else {
+          row.time_worked = '-';
+        }
+        if (items[i].u_ci_class_filter  && items[i].u_ci_class_filter !== 'null') {
+          row.classfilter = items[i].u_ci_class_filter;
+        } else {
+          row.classfilter = '';
+        }
+        if (items[i].cmdb_ci && items[i].cmdb_ci.display_value !== null) {
+          row.cmdbci  = items[i].cmdb_ci.display_value;
+        } else {
+          row.cmdbci  = '';
+        }
+
+        reqItems.push(row);
+      }
+
+      return reqItems
+    }
+    else{
+     return {}
+    }
+    });
+  }catch(err){
+    console.log("err")
+    
+  }
+}
+
+module.exports={
+    dataList:dataList
+}
